56a536c64f642cd71c259dd3a3e69bd9a9991332
evanskiprotich14 <evanskiprotich14@gmail.com>
2024-10-01 15:15:39 +0300

    Chatmessage

---
+++ api/api/Dtos/ChatMessage/ChatMessageUpdateDto.cs
@@ -0,0 +1,9 @@
+﻿namespace api.Dtos.ChatMessage;
+
+public class ChatMessageUpdateDto
+{
+    public int RequestID { get; set; }
+    public int SenderID { get; set; }
+    public int ReceiverID { get; set; }
+    public string MessageText { get; set; }
+}
